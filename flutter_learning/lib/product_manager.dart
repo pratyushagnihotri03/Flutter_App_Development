a713fa48c64f9fda64a000e335fa5fd31f3612fa
Pratyush Agnihotri <agnihotri@axxessio.com>
2020-06-09 20:00:26 +0200

    added to log to see the widget life cycle

--- flutter_learning/lib/product_manager.dart
+++ flutter_learning/lib/product_manager.dart
@@ -3,18 +3,38 @@ import 'package:flutter/material.dart';
 import './products.dart';
 
 class ProductManager extends StatefulWidget {
+  final String startingProduct;
+
+  ProductManager(this.startingProduct){
+    print('[ProductManager Widget] Constructor()');
+  }
+
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
+    print('[ProductManage Widget] createState()');
     return _ProductManagerState();
   }
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ['Food Tester'];
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    print('[ProductManage State] initState()');
+    _products.add(widget.startingProduct);
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    print('[ProductManage State] didUpdateWidget()');
+    super.didUpdateWidget(oldWidget);
+  }
 
   @override
   Widget build(BuildContext context) {
+    print('[ProductManage State] build()');
     return Column(
       children: [
         Container(
--- flutter_learning/lib/products.dart
+++ flutter_learning/lib/products.dart
@@ -3,17 +3,19 @@ import 'package:flutter/material.dart';
 class Products extends StatelessWidget {
   final List<String> products;
 
-  Products(this.products);
+  Products(this.products) {
+    print('[Products Widget] Constructor()');
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    print('[Products Widget] build()');
     return Column(
       children: products
           .map((e) => Card(
                 child: Column(
                   children: <Widget>[
-                    Image.asset('assets/indianFood.jpg'),
+                    // Image.asset('assets/indianFood.jpg'),
                     Text(e)
                   ],
                 ),
